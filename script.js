22c0e392688976595fe40e632213cc7f51c8a5a6
Nathaniel Swan <cygnet1.999@gmail.com>
2023-04-15 19:27:50 -0400

    added fancy

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const h2 = document.createElement('h2');
 
-h2.textContent = 'added by javascript';
+h2.textContent = '"Exorcizamus te, omnis immundus spiritus, omnis satanica potestas, omnis incursio infernalis adversarii, omnis legio, omnis congregatio et secta diabolica. Ergo, omnis legio diabolica, adiuramus te... cessa decipere humanas creaturas, eisque æternæ perditionìs venenum propinare... Vade, satana, inventor et magister omnis fallaciæ, hostis humanæ salutis... Humiliare sub potenti manu Dei; contremisce et effuge, invocato a nobis sancto et terribili nomine... quem inferi tremunt... Ab insidiis diaboli, libera nos, Domine. Ut Ecclesiam tuam secura tibi facias libertate servire, te rogamus, audi nos.';
 
 document.querySelector('body').appendChild(h2);
 
